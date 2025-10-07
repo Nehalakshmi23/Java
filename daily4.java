3a94f613e05d79817c02ceb33664c98c6e79dc40
Nehalakshmi23 <117349215+Nehalakshmi23@users.noreply.github.com>
2025-10-07 10:29:59 +0530

    Add files via upload

---
+++ daily4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class daily4{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int w= sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int num = sc.nextInt();
+
+        System.out.println(((w*x)-(y*w))-100);
+   
+       
+}}
